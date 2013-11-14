892688c66251efcabaa26c941ce5e90f3f3d58df
Lawrence Kesteloot <lk@teamten.com>
2013-11-14 10:20:51 -0800

    Log status code.

--- webutil/logging.go
+++ webutil/logging.go
@@ -6,17 +6,45 @@ import (
 	"time"
 )
 
-func timeRequest(startTime time.Time, r *http.Request) {
+// A ResponseWriter proxy that keeps track of the status code.
+type StatusResponseWriter struct {
+	w http.ResponseWriter
+	statusCode int
+}
+
+func (s *StatusResponseWriter) Header() http.Header {
+	return s.w.Header()
+}
+
+func (s *StatusResponseWriter) Write(data []byte) (int, error) {
+	return s.w.Write(data)
+}
+
+func (s *StatusResponseWriter) WriteHeader(statusCode int) {
+	s.statusCode = statusCode
+	s.w.WriteHeader(statusCode)
+}
+
+func (s *StatusResponseWriter) StatusCode() int {
+	if s.statusCode == 0 {
+		// It's 0 if it's not been set, but then the http lib changes that to 200.
+		return 200
+	}
+	return s.statusCode
+}
+
+func timeRequest(w *StatusResponseWriter, startTime time.Time, r *http.Request) {
 	elapsed := time.Since(startTime)
-	log.Printf("%s %s (%s)", r.Method, r.URL, elapsed)
-	// Unfortunately I don't see a way to get the status code from the response here.
+	log.Printf("%s %s %d (%s)", r.Method, r.URL, w.StatusCode(), elapsed)
 }
 
 // Handler that logs request URLs and the time spent in them.
 func LoggingHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer timeRequest(time.Now(), r)
+		statusW := StatusResponseWriter{w, 0}
 
-		handler.ServeHTTP(w, r)
+		defer timeRequest(&statusW, time.Now(), r)
+
+		handler.ServeHTTP(&statusW, r)
 	})
 }
