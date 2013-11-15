3e62df622299e236fe0f89a673132159e8352d91
Lawrence Kesteloot <lk@teamten.com>
2013-11-15 10:41:54 -0800

    Add comments.

--- dbutil/db.go
+++ dbutil/db.go
@@ -44,6 +44,8 @@ func (tx *Tx) QueryRow(query string, args ...interface{}) *sql.Row {
 	return ((*sql.Tx)(tx)).QueryRow(query, args...)
 }
 
+// Call the callback with the database object. Closes the database
+// object after the callback returns.
 func DoWithDb(credentials string, callback func(*sql.DB)) {
 	db, err := sql.Open("postgres", credentials)
 	if err != nil {
@@ -55,6 +57,8 @@ func DoWithDb(credentials string, callback func(*sql.DB)) {
 	callback(db)
 }
 
+// Call the callback within a transaction. Rolls the transaction
+// back if there's a panic.
 func DoWithTx(credentials string, callback func(*Tx)) {
 	DoWithDb(credentials, func(db *sql.DB) {
 		tx, err := db.Begin()
@@ -77,6 +81,9 @@ func DoWithTx(credentials string, callback func(*Tx)) {
 	})
 }
 
+// Return the version number of the schema in the database. If there
+// is no schema, creates a basic schema (including the global_timestamp
+// domain) and returns 0.
 func GetSchemaVersion(tx *Tx) (version int) {
 	// First see if the schema_tracker table exists.
 	var rowCount int
@@ -94,12 +101,14 @@ func GetSchemaVersion(tx *Tx) (version int) {
 		// Relation doesn't exist, create it.
 		log.Printf("The schema_tracker relation doesn't exist. Creating it.")
 
-		// This is a double float in seconds, showing 0 digits of sub-second
+		// This is an 8-byte int in microseconds, showing 0 digits of sub-second
 		// precision, and containing a time zone. We do want the "with time zone"
 		// qualifier, because it forces the timestamp to be stored in UTC time.
 		// Without it, it stores whatever value you gave it, which might be local
 		// time or whatever. See sections 8.5.1.3 (TIMESTAMP) and 9.9.3 (WITH TIME
-		// ZONE) of the PostgreSQL reference manual.
+		// ZONE) of the PostgreSQL reference manual. When getting a global_timestamp
+		// out of the database, be sure to call UTC() on the time.Time so that you
+		// won't be thrown off by the timezone of the computer.
 		tx.MustExec(`
 			CREATE DOMAIN global_timestamp
 			AS timestamp(0) with time zone`)
